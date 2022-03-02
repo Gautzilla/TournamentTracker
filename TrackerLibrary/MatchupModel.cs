20149e1662a2ba1fbd3ae3da603768eca7934df3
Gautzilla <gauthier.berthomieu@gmail.com>
2022-03-02 11:50:39 +0100

    Add XML comments to each class

--- TrackerLibrary/MatchupModel.cs
+++ TrackerLibrary/MatchupModel.cs
@@ -8,8 +8,17 @@ namespace TrackerLibrary
 {
     public class MatchupModel
     {
+        /// <summary>
+        /// Represents the entries of the two teams competing in this particular matchup.
+        /// </summary>
         public List<MatchupEntryModel> Entries { get; set; } = new();
+        /// <summary>
+        /// Represents the winner team of this particular matchup.
+        /// </summary>
         public TeamModel Winner { get; set; }
+        /// <summary>
+        /// Represents the round in which this matchup takes place.
+        /// </summary>
         public int MatchupRound { get; set; }
     }
 }
--- TrackerLibrary/PrizeModel.cs
+++ TrackerLibrary/PrizeModel.cs
@@ -8,9 +8,21 @@ namespace TrackerLibrary
 {
     public class PrizeModel
     {
+        /// <summary>
+        /// Represents the number of the place, 1 being the winner of the tournament.
+        /// </summary>
         public int PlaceNumber { get; set; }
+        /// <summary>
+        /// Represents the name of this particular place.
+        /// </summary>
         public string PlaceName { get; set; }
+        /// <summary>
+        /// Represents the prize given to this particular place.
+        /// </summary>
         public Decimal PrizeAmount { get; set; }
+        /// <summary>
+        /// Represents the prize given to this particular place as a percentage of the total entry fees.
+        /// </summary>
         public double PrizePercentage { get; set; }
     }
 }
